976b79f000e856aeaf629dd19a9959b38738cd26
G-Doan <doan9571@student.acitech.org>
2026-01-30 10:57:00 -0500

    Almost Done

--- doan_dicepoker_main.py
+++ doan_dicepoker_main.py
@@ -18,7 +18,7 @@
 import inquirer3
 import os
 from dice import Dice
-from sort_hand import hand_sort
+from collections import Counter
 
 # Important variables
 global dice
@@ -54,39 +54,90 @@ def keep_select(kept_list):
     return kept_list
 
 
+def game_play():
+    # Some Global Variables
+    global rolled
+    rolled = dice.roll_dice()
+    global kept_dice
+    kept_dice = []
+    # Important variables for the code to run correctly
+    rolls = 3
+    playing = True
+    while playing:
+        if rolls == 1:
+            playing = False
+        os.system('cls' if os.name == 'nt' else 'clear')  # Code to clear old text
+        print(f"Rolls Remaining: {rolls}")  # Print for the number of rolls left
+        print(f"Your Roll: {rolled}")  # Print to show dice roll
+        score = get_score(rolled)  # Calls on function to get the value of the hand
+        hand_type = hand_evaluate(score)  # Calls on function to get the type of hand
+        print(f"You got {hand_type}\n")  # Print for the type of hand you have
+        # Inquirer menu for the actual game
+        questions = [
+            inquirer3.List('menu',
+                           message="Select action",
+
+                           choices=['Roll Dice', 'Keep Dice'])
+        ]
+        answers = inquirer3.prompt(questions)
+        select = "{}".format(answers['menu'])
+        # Code for what the user selects
+        match select:
+            # If user chooses to roll the dice again
+            case "Roll Dice":
+                rolls -= 1
+                os.system('cls' if os.name == 'nt' else 'clear')  # Code to clear old text
+                rolled = dice.roll_dice()
+                print(f"Your Roll: {rolled}\n")  # Print to show dice roll
+            case "Keep Dice":
+                os.system('cls' if os.name == 'nt' else 'clear')  # Code to clear old text
+                print(f"Your Roll: {rolled}\n")  # Print to show dice roll
+                kept_dice = keep_select(kept_dice)  # Code to get call on select kept dice function
+                # Code to keep the dice in dice class
+                for i in range(len(kept_dice)):
+                    dice.keep_dice(int(kept_dice[i]))
+    os.system('cls' if os.name == 'nt' else 'clear')  # Code to clear old text
+    print(f"Final Hand: {rolled}")
+    score = get_score(rolled)  # Calls on function to get the value of the hand
+    hand_type = hand_evaluate(score)
+    print(f"You got {hand_type}\n")
+
+
+# Function to ge the point worth of your hand
 def get_score(hand):
-    # print(hand)
-    # print(hand.count(1))
-    for i in range(len(hand)):
-        hand_value = hand.count(hand[i])
-        print(f"I:{i}: ({hand[i]}): {hand_value}")
-        if hand_value == 5:
-            temp_value = hand_value
-            # print("kind5")
-        elif hand_value == 4:
-            temp_value = hand_value
-            # print("kind4")
-        elif hand_value == 3:
-            temp_value = hand_value
-            # print("kind3")
-        elif hand_value == 2:
-            temp_value = hand_value
-            # print("kind2")
-        elif hand_value == 1:
-            temp_value = hand_value
-            straight = straight_check(hand)
-            if straight == "yes":
-                print("straight")
-    # kind5 = 7
-    # kind4 = 6
-    # full = 5
-    # straight = 4
-    # kind3 = 3
-    # pair2 = 2
-    # kind2 = 1
-    # bust = 0
+    sorted_hand = sorted(hand)
+    counts = Counter(hand)
+    hand_value = sorted(counts.values(), reverse=True)  # To get how many times a number appears
+    unique_values = sorted(counts.keys(), reverse=True)  # To get the value of the unique die
+    straight = straight_check(sorted_hand)  # Calls on the function for checking for a straight
+    if hand_value[0] == 5:
+        score_value = 7
+    elif hand_value[0] == 4:
+        score_value = 6
+    elif hand_value == [3, 2]:
+        score_value = 5
+    elif hand_value[0] == 3:
+        score_value = 3
+    elif hand_value == [2, 2, 1]:
+        score_value = 2
+    elif hand_value[0] == 2:
+        score_value = 1
+    elif hand_value[0] == 1:
+        if straight == "yes":
+            score_value = 4
+        else:
+            score_value = 0
+    return score_value
 
 
+# Function to get the type of hand
+def hand_evaluate(hand_score):
+    hand_types = ["Bust", "2 of a kind", "2 Pairs", "3 of a kind",
+                  "Straight", "Full House", "4 of a kind", "5 of a kind"]
+    return hand_types[hand_score]
+
+
+# Function that checks if the hand is a straight
 def straight_check(check):
     global straight1, straight2
     if check == straight1:
@@ -100,10 +151,8 @@ def straight_check(check):
         return straight
 
 
+# The main function used
 def main():
-    # global variables
-    global kept_dice
-    kept_dice = []
     # To give initial rolled dice and show user values
     while True:
         # Inquirer menu to start the Game
@@ -118,46 +167,11 @@ def main():
         # Match select for the start menu
         match select:
             case "Start Game":
-                # global variable for dice roll
-                global rolled
-                rolled = dice.roll_dice()
-                rolled = hand_sort(rolled)
-                print(f"Your Roll: {rolled}\n")  # Print to show dice roll
-                # get_score(rolled)
-                # while loop for the actual game
-                while True:
-                    os.system('cls' if os.name == 'nt' else 'clear')  # Code to clear old text
-                    print(f"Your Roll: {rolled}\n")  # Print to show dice roll
-                    get_score(rolled)
-                    # Inquirer menu for the actual game
-                    questions = [
-                        inquirer3.List('menu',
-                                       message="Select dice to keep",
+                game_play()  # Calls on the function that actually plays the game
 
-                                       choices=['Roll Dice', 'Keep Dice'])
-                    ]
-                    answers = inquirer3.prompt(questions)
-                    select = "{}".format(answers['menu'])
-                    # Code for what the user selects
-                    match select:
-                        # If user chooses to roll the dice again
-                        case "Roll Dice":
-                            os.system('cls' if os.name == 'nt' else 'clear')  # Code to clear old text
-                            rolled = dice.roll_dice()
-                            rolled = hand_sort(rolled)
-                            print(f"Your Roll: {rolled}\n")  # Print to show dice roll
-                            get_score(rolled)
-                        case "Keep Dice":
-                            os.system('cls' if os.name == 'nt' else 'clear')  # Code to clear old text
-                            print(f"Your Roll: {rolled}\n")  # Print to show dice roll
-                            kept_dice = keep_select(kept_dice)  # Code to get call on select kept dice function
-                            # Code to keep the dice in dice class
-                            for i in range(len(kept_dice)):
-                                dice.keep_dice(int(kept_dice[i]))
             case "Exit":
                 exit()
 
 
 if __name__ == '__main__':
     main()
-
